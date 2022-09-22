0e095474d1e3a4e0b7b17d39b52fdedd84c04b4b
Эльвира Челнокова <elvira.chelnokova05@outlook.com>
2022-09-22 07:47:21 +0300

    text edit

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace pz_5
+﻿using System.Globalization;
+using System.Runtime.Serialization.Formatters;
+
+namespace pz_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("fgh");
+            //Console.WriteLine("задание 1");
+            ////for (int i = 20; i <= 80; i += 3)
+            //// {
+            ////     Console.WriteLine(i);
+            //// }
+            //Console.WriteLine("задание 2");
+            //char a = 'з';
+            //for (int  i = 0 ; i < 6; i ++)
+            //{
+            //    Console.Write(a);
+            //    a++;
+            //}
+            //Console.WriteLine("задание 3");
+            //for (var i = 0; i < 8; i++)
+            //{
+            //    for (var j = 0; j < 6; j++)
+            //    {
+            //        Console.Write('#');
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            //Console.ReadKey();
+            //Console.WriteLine("задание 4");
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 19 == 0)
+            //        Console.Write($"{i} ") ;
+            //}
+            for (int i = 0, j = 99; j - i == 21; i++, j--)
+            {
+                Console.WriteLine(i + j);
+            }
+
         }
     }
 }
