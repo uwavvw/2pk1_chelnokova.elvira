d8b1afb934c3bcc2a24b528cae874714e31ab0b2
Эльвира Челнокова <elvira.chelnokova05@outlook.com>
2022-09-26 11:06:42 +0300

    ))

---
+++ pz_6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            int result = 1;
+            while (i != fact)
+            {
+                i++;
+                    result *= i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
