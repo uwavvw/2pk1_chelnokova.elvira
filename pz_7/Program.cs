df60b5dd1d3ed2f50ab9b6ecef7bb363fa3555fc
Эльвира Васильевна Челнокова <chelnokova.ev1672@oksei.onmicrosoft.com>
2022-10-03 21:25:06 +0500

    индекс не получился

---
+++ pz_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            int[] b = new int[100];
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = r.Next(0, 100);
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
