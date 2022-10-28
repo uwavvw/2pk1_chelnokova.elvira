93b20d0853bccd6d80b2e4fdf84bb30e73bf9c8a
Эльвира Васильевна Челнокова <chelnokova.ev1672@oksei.onmicrosoft.com>
2022-10-28 19:31:46 +0500

    22 вариант

---
+++ pz10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите строку");
+            string str = Console.ReadLine();
+            int one = 0;
+            int two = 0;
+            int three = 0;
+
+            if (str.Length >= 6)
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsUpper(str[i]))
+                    {
+                        one++;
+
+                    }
+                    if (char.IsLower(str[i]))
+                    {
+                        two++;
+                    }
+                    if (str.Contains('.') || str.Contains('!') || str.Contains('_') || str.Contains('-'))
+                    {
+                        three++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("пароль не соответствует");
+                    }
+                }
+            if (one>0 && two > 0 && three>0)
+            {
+                Console.WriteLine("пароль соответсвует");
+            }
+                
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pz_10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Привет мир";
+            Console.WriteLine(hello);
+            Console.WriteLine("Добро пожаловать в C#!");
+            Console.WriteLine("Пока мир...");
+            Console.WriteLine(24.5);
+        }
+
+    }
+}
+
