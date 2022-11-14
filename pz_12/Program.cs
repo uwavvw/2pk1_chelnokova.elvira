84db6acd3e6a8b0fb43365ab07d73890b1f6c08f
Эльвира Челнокова <elvira.chelnokova05@outlook.com>
2022-11-14 09:17:39 +0300

    22в

---
+++ pz_12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_12
+{
+    internal class Program
+    {
+        static void GetMinMax(double x, double y, out double z )
+        {
+            
+            if (x > y)
+            {
+                z = x;
+            }
+            else if (x < y)
+            {
+                z = y;
+            }
+            z = Math.Max(x, y);
+            Console.WriteLine(z);
+        }
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double z;
+            GetMinMax(a, b, out z);
+            double max1 = Math.Max(a, b);
+            double d = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double z1;
+            GetMinMax(d, c, out z1);
+            double max2 = Math.Max(d, c);
+            double z2;
+            GetMinMax(max1, max2, out z2);
+            double max3 = Math.Max(max1, max2);
+            
+            
+            Console.ReadLine();
+
+        }
+    }
+}
