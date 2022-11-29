09d5f5444544aa5c31b2cb8fbebfb518874a7fc4
Эльвира Васильевна Челнокова <chelnokova.ev1672@oksei.onmicrosoft.com>
2022-11-29 18:49:45 +0500

    22в

---
+++ pz_16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите S: ");
+            string a = Console.ReadLine();
+            if (a == "S")
+            {
+                using (StreamReader r = new StreamReader("text1.txt"))
+                {
+                    while (!r.EndOfStream)
+                    {
+                        string b = r.ReadLine();
+                        if (b.Contains(a))
+                        {
+                            Console.WriteLine(b);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("введена не та буква");
+            }
+            Console.ReadLine();
+        }
+    }
+}
