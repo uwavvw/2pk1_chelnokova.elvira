33f78c9e3446c8bdb5b918ef7030ead0b7b97cc4
Эльвира Васильевна Челнокова <chelnokova.ev1672@oksei.onmicrosoft.com>
2022-12-14 21:22:56 +0500

    4 вариант

---
+++ pz_17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_17
+{
+    internal class Program
+    {
+        unsafe
+        static void Main(string[] args)
+        {
+            
+            int a;
+            
+            char** b;
+            char* c = (char*)&a;
+            b = &c;
+            **b = '+';
+            *(*b + 1) = '*';
+            Console.WriteLine($"содержимое переменной а { a}");
+            Console.WriteLine($"значение типа char {**b}");
+            Console.WriteLine($"значение типа char {*(*b+1)}");
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
