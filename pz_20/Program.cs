388f892fd87157d9990321ca81dfcb9a07c1158c
Эльвира Васильевна Челнокова <chelnokova.ev1672@oksei.onmicrosoft.com>
2023-01-19 17:27:48 +0500

    ...

---
+++ pz_20/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            string text = Console.ReadLine();
+            char[] myArray = text.ToCharArray();
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] >= 'А' && myArray[i] <= 'Я' || myArray[i] >= 'A' && myArray[i] <= 'Z')
+                {
+                    Console.Write(Char.ToLower(myArray[i]) + "");
+                }
+                else if (myArray[i] >= 'а' && myArray[i] <= 'я' || myArray[i] >= 'a' && myArray[i] <= 'z')
+                {
+                    Console.Write(Char.ToUpper(myArray[i]) + "");
+                }
+                else
+                {
+                    Console.WriteLine("Введен неправильный символ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
