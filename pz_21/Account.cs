98a73c3cc76703601b3ad0cdfe2b3e2d9e5fae95
Эльвира Васильевна Челнокова <chelnokova.ev1672@oksei.onmicrosoft.com>
2023-02-02 13:02:17 +0500

    22В

---
+++ pz_21/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class Account
+    {
+        public int ID;
+        public string email;
+        public string login;
+        public string pass;
+        public string registrationDate;
+
+        public Account()
+        {
+            ID = 000;
+            email = "elvira.chelnokova05@gmail.com";
+            login = "89234990321";
+            pass = "Okei2023!";
+            registrationDate = "02.02.2023";
+            Console.WriteLine("Информация об аккаунте студента: ");
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"ID студента: {ID}\nЕmail студента: {email}\nЛогин студента: {login}\nПароль студента: {pass}\nДата регистрации студента:{registrationDate}");
+        }
+    }
+}
