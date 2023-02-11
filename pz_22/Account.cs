eddfb33a1a6297965cce4fde01ccde369c43d5be
Эльвира Васильевна Челнокова <chelnokova.ev1672@oksei.onmicrosoft.com>
2023-02-11 18:01:34 +0500

    22в

--- pz_22/Account.cs
+++ pz_22/Account.cs
@@ -9,53 +9,35 @@ namespace pz_22
 {
     internal class Account
     {
-        public int id;
+        private int id;
         public string pattern = @"^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!#@]).{8,}$";
-        //1
-        private string email1;
-        private string login1;
-        private string pass1;
-        DateTime registrationDate1;
-        //2
-        private string email2;
-        private string login2;
-        private string pass2;
-        DateTime registrationDate2;
-        //3
-        private string email3;
-        private string login3;
-        private string pass3;
-        DateTime registrationDate3;
         DateTime minDate = new DateTime(2000, 01, 01);
         DateTime maxDate = new DateTime(2022, 01, 01);
-       
-        public Account()
-        {
-            Id = int.Parse(Console.ReadLine());
-            //1 
-            Email1 = "elvira.chelnokova05@gmail.com";
-            Login1 = "89234990321"; 
-            Pass1 = "Okei2023!";
-            RegistrationDate1 = new DateTime (2021, 06, 12);
-            //2
-            Email2 = "ivan.asidjaid@gmail.com";
-            Login2 = "89542342351";
-            Pass2 = "Password3@";
-            RegistrationDate2 = new DateTime(2019, 10, 10);
-            //3
-            Email3 = "mishaaaxbemid@gmail.com";
-            Login3 = "89568935789";
-            Pass3 = "Vjsdf239#";
-            RegistrationDate3 = new DateTime(2020, 04, 07);
+        public static int count;
+        private string email;
+        private string login;
+        private string pass;
+        static DateTime registrationDate;
 
+        public static void accountCheck()
+        {
+            DateTime date = new DateTime(2020, 01, 01);
+            if (date < registrationDate)
+            {
+                count++;
+                
+                Console.WriteLine($"ID аккаунта, который зарегестировался после 2020 года: {count}");
+            }
+            else
+                 Console.WriteLine("аккаунт зарегестирован раньше 2020 года");
+            
         }
-        
-        
         public int Id
         {
             get { return id; }
             set
-            {   if (value == 0 || value > 3)
+            {
+                if (value == 0 || value > 3)
                 {
                     Console.WriteLine("ID не совпадает");
                 }
@@ -67,52 +49,22 @@ namespace pz_22
         }
         public string Pass1
         {
-            get { return pass1; }
-            set 
-            {
-                if (Regex.IsMatch(value, pattern)) //создаем цикл в котором проверяем обнаружено ли в указанной входной строке соответствие заданному
-                {
-                    pass1 = value;
-                }
-                else
-                {
-                    Console.WriteLine("пароль не подходит");
-                }
-            }
-        }//свойство для проверки пароля акк 01
-        public string Pass2
-        {
-            get { return pass1; }
+            get { return pass; }
             set
             {
                 if (Regex.IsMatch(value, pattern)) //создаем цикл в котором проверяем обнаружено ли в указанной входной строке соответствие заданному
                 {
-                    pass1 = value;
+                    pass = value;
                 }
                 else
                 {
                     Console.WriteLine("пароль не подходит");
                 }
             }
-        }//свойство для проверки пароля акк 02
-        public string Pass3
-        {
-            get { return pass3; }
-            set
-            {
-                if (Regex.IsMatch(value, pattern)) //создаем цикл в котором проверяем обнаружено ли в указанной входной строке соответствие заданному
-                {
-                    pass3 = value;
-                }
-                else
-                {
-                    Console.WriteLine("пароль не подходит");
-                }
-            }
-        }//свойство для проверки пароля акк 03
+        }//свойство для проверки пароля акк 
         public string Login1
         {
-            get { return login1; }
+            get { return login; }
             set
             {
                 if (value == "") //создаем цикл в котором проверяем обнаружено ли в указанной входной строке соответствие заданному
@@ -121,43 +73,13 @@ namespace pz_22
                 }
                 else
                 {
-                    login1 = value; 
+                    login = value;
                 }
             }
-        }//свойство для проверки логина акк 01
-        public string Login2
-        {
-            get { return login2; }
-            set
-            {
-                if (value == "") //создаем цикл в котором проверяем обнаружено ли в указанной входной строке соответствие заданному
-                {
-                    Console.WriteLine("логина не существует");
-                }
-                else
-                {
-                    login2 = value;
-                }
-            }
-        }//свойство для проверки логина акк 02
-        public string Login3
-        {
-            get { return login3; }
-            set
-            {
-                if (value == "") //создаем цикл в котором проверяем обнаружено ли в указанной входной строке соответствие заданному
-                {
-                    Console.WriteLine("логина не существует");
-                }
-                else
-                {
-                    login3 = value;
-                }
-            }
-        }//свойство для проверки логина акк 03
+        }//свойство для проверки логина акк 
         public string Email1
         {
-            get { return email1; }
+            get { return email; }
             set
             {
                 if (value == "") //создаем цикл в котором проверяем обнаружено ли в указанной входной строке соответствие заданному
@@ -166,99 +88,25 @@ namespace pz_22
                 }
                 else
                 {
-                    email1 = value;
+                    email = value;
                 }
             }
-        }//свойство для проверки почты акк 01
-        public string Email2
-        {
-            get { return email2; }
-            set
-            {
-                if (value == "") //создаем цикл в котором проверяем обнаружено ли в указанной входной строке соответствие заданному
-                {
-                    Console.WriteLine("почта не существует");
-                }
-                else
-                {
-                    email2 = value;
-                }
-            }
-        }//свойство для проверки почты акк 02
-        public string Email3
-        {
-            get { return email3; }
-            set
-            {
-                if (value == "") //создаем цикл в котором проверяем обнаружено ли в указанной входной строке соответствие заданному
-                {
-                    Console.WriteLine("почта не существует");
-                }
-                else
-                {
-                    email3 = value;
-                }
-            }
-        }//свойство для проверки почты акк 03
+        }//свойство для проверки почты акк 
         public DateTime RegistrationDate1
         {
-            get { return registrationDate1; }
-            set
-            {
-                if (value > minDate && value < maxDate) 
-                {
-                    registrationDate1 = value; 
-                }
-                else
-                {
-                    Console.WriteLine("дата не входит в диапазон");
-                }
-            }
-        }//свойство для проверки даты акк 01
-        public DateTime RegistrationDate2
-        {
-            get { return registrationDate2; }
-            set
-            {
-                if (value > minDate && value < maxDate) 
-                {
-                    registrationDate2 = value;
-                }
-                else
-                {
-                    Console.WriteLine("дата не входит в диапазон");
-                }
-            }
-        }//свойство для проверки даты акк 02
-        public DateTime RegistrationDate3
-        {
-            get { return registrationDate3; }
+            get { return registrationDate; }
             set
             {
                 if (value > minDate && value < maxDate)
                 {
-                    registrationDate3 = value;
+                    registrationDate = value;
                 }
                 else
                 {
                     Console.WriteLine("дата не входит в диапазон");
                 }
             }
-        }//свойство для проверки даты акк 03
+        }//свойство для проверки даты акк 
 
-
-
-        public void PrintInfo()
-        {
-            if(id == 1)
-            Console.WriteLine($"ID студента: {id}\nЕmail студента: {email1}\nЛогин студента: {login1}\nПароль студента: {pass1}\nДата регистрации студента: {registrationDate1}");
-            else if (id == 2 )
-                Console.WriteLine($"ID студента: {id}\nЕmail студента: {email2}\nЛогин студента: {login2}\nПароль студента: {pass2}\nДата регистрации студента: {registrationDate2}");
-            else if (id == 3)
-                Console.WriteLine($"ID студента: {id}\nЕmail студента: {email3}\nЛогин студента: {login3}\nПароль студента: {pass3}\nДата регистрации студента: {registrationDate3}");
-            else
-                Console.WriteLine("аккаунта не существует");
-        }
-        
     }
 }
--- pz_22/Program.cs
+++ pz_22/Program.cs
@@ -12,11 +12,30 @@ namespace pz_22
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите ID студента ( 1 - 3 ) ");
-            
-            Account acc = new Account();
-            acc.PrintInfo();
-            
+            Account acc1 = new Account();
+            acc1.Id = 1;
+            acc1.Email1 = "elvira.chelnokova05@gmail.com";
+            acc1.Login1 = "89234990321";
+            acc1.Pass1 = "Okei2023!";
+            acc1.RegistrationDate1 = new DateTime(2021, 06, 09);
+            Account.accountCheck();
+
+            Account acc2 = new Account();
+            acc2.Id = 2;
+            acc2.Email1 = "ivan.asidjaid@gmail.com";
+            acc2.Login1 = "89542342351";
+            acc2.Pass1 = "Password3@";
+            acc2.RegistrationDate1 = new DateTime(2021, 06, 12);
+            Account.accountCheck();
+
+            Account acc3 = new Account();
+            acc3.Id = 2;
+            acc3.Email1 = "mishaaaxbemid@gmail.com";
+            acc3.Login1 = "89568935789";
+            acc3.Pass1 = "Vjsdf239#";
+            acc3.RegistrationDate1 = new DateTime(2021, 04, 07);
+            Account.accountCheck();
+
             Console.ReadKey();
         }
     }
