dae2409fdc7c55f57c0fec4b3745d434011f08ab
Эльвира Васильевна Челнокова <chelnokova.ev1672@oksei.onmicrosoft.com>
2023-03-18 15:35:08 +0500

    26.2 не до конца, 26.3 начато, не сделано

--- pz_26.1/MainWindow.xaml.cs
+++ pz_26.1/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace pz_26._1
 {
@@ -24,17 +26,58 @@ namespace pz_26._1
         
         public MainWindow()
         {
-            
             InitializeComponent();
-            WorkFile.CreateFile();
-
+            
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void savefile_Click(object sender, RoutedEventArgs e)
         {
-            WorkFile.CreateFile();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)| *.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filename = saveFileDialog.FileName;
+                File.WriteAllText(filename, new TextRange(RTextBox.Document.ContentStart, RTextBox.Document.ContentEnd).Text);
+            }
+        }
+        private void openfile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FileName = "Документ"; 
+            openFileDialog.DefaultExt = ".txt"; 
+            openFileDialog.Filter = "Text documents (.txt)|*.txt";            
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                string filename = openFileDialog.FileName;                
+                string bebe = File.ReadAllText(filename);
+                RTextBox.Document.Blocks.Clear();//очищаем ричбокс для открытия нового файла
+                RTextBox.Document.Blocks.Add(new Paragraph(new Run(bebe)));
+            }
+        }
+        private void deletefile_Click(object sender, RoutedEventArgs e)
+        {
+            RTextBox.Document.Blocks.Clear();   
+        }
+        private void newfile_Click(object sender, RoutedEventArgs e)
+        {
+            NewFile newFile = new NewFile();
+            newFile.ShowDialog();
+            
+        }
+        private void UpdateCursorPosition()
+        {
+            //int row = RTextBox.GetLineIndexFromCharacterIndex(RTextBox.CaretIndex);
+            //int col = RTextBox.CaretIndex - RTextBox.GetLineIndexFromCharacterIndex(row);
+            //cursorPosition.Text = $"строка: {row + 1} столбец: {col + 1}";
+            
+           
         }
 
 
+        private void cursorPosition_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
     
 }
---
+++ pz_26.1/NewFile.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace pz_26._1
+{
+    /// <summary>
+    /// Логика взаимодействия для NewFile.xaml
+    /// </summary>
+    public partial class NewFile : Window
+    {
+        public NewFile()
+        {
+            InitializeComponent();
+        }
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            string baba = fileNameBox.Text;
+            FileStream file1 = new FileStream(@"C:\Users\эля\Source\Repos\2pk1_chelnokova.elvira\pz_26.1\data\" + baba + ".txt", FileMode.CreateNew);            
+        }
+        public string FileName
+        {
+            get { return fileNameBox.Text; }
+        }   
+    }
+}
