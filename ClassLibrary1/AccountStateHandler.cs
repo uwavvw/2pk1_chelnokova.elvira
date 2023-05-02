44d8cc7b8de10494d1f2add8c00dffe5a524474f
Эльвира Челнокова <elvira.chelnokova05@outlook.com>
2023-05-02 13:10:04 +0500

    библиотеки

---
+++ ClassLibrary1/AccountStateHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+
+
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+        public string Massage { get; set; }
+        public int Sum { get; private set; }
+
+        public AccountEventArgs(int a, string sms)
+        {
+            Sum = a;
+            Massage = sms;
+        }
+    }
+    
+}
---
+++ ClassLibrary1/BankLibrary.cs
@@ -0,0 +1,81 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    internal interface IAccount
+    {
+        void Put(decimal sum);
+        decimal Withdraw(decimal sum);
+        
+
+
+        
+    }
+    public abstract class Account: IAccount
+    {
+        protected internal event AccountStateHandler Withdrawed;
+        protected internal event AccountStateHandler Added;
+        protected internal event AccountStateHandler Opened;
+        protected internal event AccountStateHandler Closed;
+        protected internal event AccountStateHandler Calculated;
+        protected int _id;
+        static int counter = 0;
+        protected decimal _sum;
+        protected int _percentage;
+        protected int _days = 0;
+        public Account(decimal sum, int percentage)
+        {
+            _sum = sum;
+            _percentage = percentage;
+            _id = ++counter;
+        }
+        public decimal CurrentSum
+        {
+            get { return _sum; }
+        }
+        public int Percentage
+        {
+            get { return _percentage; }
+        }
+        public int Id
+        {
+            get { return _id; }
+        }
+        private void CallEvent(AccountEventArgs e, AccountStateHandler handler)
+        {
+            if (handler != null && e != null)
+                handler(this, e);
+        }
+        // вызов отдельных событий. Для каждого события определяется свой витуальный метод
+        protected virtual void OnOpened(AccountEventArgs e)
+        {
+            CallEvent(e, Opened);
+        }
+        protected virtual void OnWithdrawed(AccountEventArgs e)
+        {
+            CallEvent(e, Withdrawed);
+        }
+        protected virtual void OnAdded(AccountEventArgs e)
+        {
+            CallEvent(e, Added);
+        }
+        protected virtual void OnClosed(AccountEventArgs e)
+        {
+            CallEvent(e, Closed);
+        }
+        protected virtual void OnCalculated(AccountEventArgs e)
+        {
+            CallEvent(e, Calculated);
+        }
+        public virtual void Put(decimal sum)
+        {
+            _sum += sum;
+            OnAdded(new AccountEventArgs("На счет поступило " + sum, sum));
+        }
+    }
+}
